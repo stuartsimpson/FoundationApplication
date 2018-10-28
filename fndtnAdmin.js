100bfddc9e6dc22e7497fc886af8c4c01334d6b0
Stuart Simpson <stuart.simpson@musicpro.com>
2018-10-28 12:02:18 -0600

    updating Seed Data.  this seed data will have the version numbers of each record.

--- fndtnAdmin.js
+++ fndtnAdmin.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const readline = require('readline');
 const mongoose = require('mongoose');
+const { exec } = require('child_process');
 
 const config = require('./server/config.js');
 
@@ -91,15 +92,33 @@ function clearSeedData(model) {
     });
 }
 
+function exportSeedData(model){
+    // TODO: Need to change the mongoexport command to a variable in either the config file or in the fndtnAdmin script
+    exec(`mongoexport.exe --db ${config.mongo.db} --collection ${model} --out ./seedData/${model}.json`, (error, stdout, stderr) => {
+        if(error) {
+            console.log(`Could not export Seed Data: ${model}`);
+            console.log(error);
+        } else {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        }
+    })
+}
+
 switch (process.argv[2]) {
     case 'loadSeedData':
         {
-            loadSeedData(process.argv[3])
+            loadSeedData(process.argv[3]);
             break;
         }
     case 'clearSeedData':
         {
-            clearSeedData(process.argv[3])
+            clearSeedData(process.argv[3]);
+            break;
+        }
+    case 'exportSeedData':
+        {
+            exportSeedData(process.argv[3]);
             break;
         }
     case 'loadAllSeedData':
@@ -122,6 +141,17 @@ switch (process.argv[2]) {
             clearSeedData('fndtnRoleResources');
             clearSeedData('fndtnRoles');
             clearSeedData('fndtnUsers');
+            break;
+        }
+    case 'exportAllSeedData':
+        {
+            exportSeedData('fndtnAccessControl');
+            exportSeedData('fndtnApplicationErrors');
+            exportSeedData('fndtnMenus');
+            exportSeedData('fndtnResources');
+            exportSeedData('fndtnRoleResources');
+            exportSeedData('fndtnRoles');
+            exportSeedData('fndtnUsers');
             break;
         }
     default:
